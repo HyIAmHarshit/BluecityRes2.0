b3f42981c6f1f8527b9d86420d88c4306ba0f7a9
HyIAmHarshit <khandelwalharshit2024@gmail.com>
2026-03-25 16:56:58 +0530

    update

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+function connectToDB(){
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("Server is connected to db");
+    }).catch(err=>{
+        console.log("Error occure during connection db"+err)
+        process.exit(1);
+    })
+}
+
+module.exports=connectToDB;
---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,"Email is required for creating a user"],
+        trim:true,
+        lowercase:true,
+        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,"Invalid Email address"],
+        unique:[true,"Email already exists"]
+    },
+    name:{
+        type:String,
+        required:[true,"Name is required for creating an account"]
+    },
+    password:{
+        type:String,
+        required:[true,"Password is requireed for creating an account"],
+        minlength:[6,"password should be contain more then 6 character"],
+        select:false
+    },
+    timestamp:true
+
+})
+userSchema.pre("save",async function(next){
+    
+})
